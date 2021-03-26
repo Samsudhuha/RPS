6322b721fa2d1bb85ba532a0e4589b0187c69701
Ryukazu <ryukazuas@gmail.com>
2021-03-26 21:24:23 +0700

    revisi table

--- resources/views/rps/cetakPDF.php
+++ resources/views/rps/cetakPDF.php
@@ -13,17 +13,17 @@
 <body>
     <div class="container">
         <table class="table table-bordered">
-            <tr>
-                <td><img src="logo.jpg" alt="" height=100 width=100></td>
-                <th colspan="6" style="text-align:center">INSTITUT TEKNOLOGI SEPULUH NOPEMBER (ITS)<br>Fakultas Teknologi Elektro dan Informatika Cerdas<br>Departemen Teknik Informatika</th>
+            <tr class="font-weight-bold text-center" style="background-color: #deeaf6;">
+                <td><img src="logo.png" alt="" height=100 width=100></td>
+                <td colspan="6">INSTITUT TEKNOLOGI SEPULUH NOPEMBER (ITS)<br>Fakultas Teknologi Elektro dan Informatika Cerdas<br>Departemen Teknik Informatika</td>
                 <td>Kode Surat</td>
             </tr>
             <tr>
-                <td colspan="8" style="text-align:center">
-                    <h4>Rencana Pembelajaran Semester</h4>
+                <td colspan="8" class="text-center" style="background-color: #deeaf6;">
+                    <h4 class="font-weight-bold">Rencana Pembelajaran Semester</h4>
                 </td>
             </tr>
-            <tr>
+            <tr class="font-weight-bold bg-light">
                 <td colspan="2">Mata Kuliah (MK)</td>
                 <td>KODE</td>
                 <td>Rumpun MK</td>
@@ -34,7 +34,7 @@
                 <!--<td></td>-->
             </tr>
             <tr>
-                <td colspan="2">Topik Dalam Pengembangan Game, Realitas Virtual & Augmentasi (TD PGRVA)</td>
+                <td colspan="2" class="font-weight-bold">Topik Dalam Pengembangan Game, Realitas Virtual & Augmentasi (TD PGRVA)</td>
                 <td>IF185932</td>
                 <td>Grafika, Interaksi, dan Games</td>
                 <td>3</td>
@@ -42,20 +42,20 @@
                 <td>3</td>
                 <td>10 Mei 2020</td>
             </tr>
-            <tr>
+            <tr class="font-weight-bold">
                 <td colspan="2" rowspan="2">OTORISASI / PENGESAHAN</td>
-                <td colspan="2">Dosen Pengembang RPS</td>
-                <td colspan="2">Koordinator RMK</td>
-                <td colspan="2">Ka PRODI</td>
+                <td colspan="2" class="bg-light">Dosen Pengembang RPS</td>
+                <td colspan="2" class="bg-light">Koordinator RMK</td>
+                <td colspan="2" class="bg-light">Ka PRODI</td>
             </tr>
             <tr>
                 <td colspan="2">Dr. Eng Darlis Herumurti</td>
                 <td colspan="2">Imam Kuswardayan, M.T</td>
                 <td colspan="2">Dr. Eng Chastine Fatichah</td>
             </tr>
-            <tr>
+            <tr class="font-weight-bold">
                 <td rowspan="5">Capaian Pembelajaran</td>
-                <td colspan="7">CPL-Prodi yang dibebankan pada MK</td>
+                <td colspan="7" class="bg-light">CPL-Prodi yang dibebankan pada MK</td>
             </tr>
             <tr>
                 <td>CPL 4</td>
@@ -66,58 +66,85 @@
                 <td colspan="6">Mampu bekerja dan berkomunikasi secara efektif baik secara individu maupun dalam kelompok</td>
             </tr>
             <tr>
-                <td colspan="7">Capaian Pembelajaran Mata Kuliah (CPMK)</td>
+                <td colspan="7" class="font-weight-bold bg-light">Capaian Pembelajaran Mata Kuliah (CPMK)</td>
             </tr>
             <tr>
                 <td>CPMK 1</td>
-                <td colspan="6">Mahasiswa mampu menganalisa dan mengklasifikasikan game berdasarkan genre, tema dan rating.</td>
+                <td colspan="6 ">Mahasiswa mampu menganalisa dan mengklasifikasikan game berdasarkan genre, tema dan rating.</td>
             </tr>
             <tr>
-                <td>Peta CPL - CP MK</td>
-                <td colspan="7">Bagian untuk Peta CPL dan CPMK</td>
+                <td class="font-weight-bold">Peta CPL - CP MK</td>
+                <td colspan="7 ">Bagian untuk Peta CPL dan CPMK</td>
             </tr>
             <tr>
-                <td>Deskripsi Singkat MK</td>
+                <td class="font-weight-bold">Deskripsi Singkat MK</td>
                 <td colspan="7">Pada mata kuliah ini mahasiswa akan belajar mengenai tahapan pengembangan sebuah game yang edukatif dan penelitian yang terkait dengan interaksi manusia komputer secara spesifik di domain realitas virtual dan augmentasi. Mahasiswa diharapkan
                     mampu merancang, menganalisis serta mengaplikasikan metode-metode pengembangan game pada permasalahan nyata serta mengangkat ide-ide segar dalam suatu penelitian interaksi manusia komputer pada domain realitas virtual dan augmentasi
                     dengan pendekatan multidisipliner baik secara mandiri atau kerjasama tim.
                 </td>
             </tr>
             <tr>
-                <td>Bahan Kajian: Materi Pembelajaran</td>
-                <td colspan="7">
+                <td>
+                    <span class="font-weight-bold">Bahan Kajian: </span>Materi Pembelajaran</td>
+                <td colspan="7 ">
                     1. HCI/New Interactive Technologies<br> 2. HCI/Mixed, Augmented and Virtual Reality
                 </td>
             </tr>
             <tr>
-                <td rowspan="3">Pustaka</td>
-                <td>Utama: </td>
-                <td colspan="6">
+                <td rowspan="2" class="font-weight-bold">Pustaka</td>
+                <td class="font-weight-bold bg-light">Utama: </td>
+                <td colspan="6 ">
                     1. The VR Book: Human-Centered Design for Virtual Reality by Jason Jerald<br> 2. 3D User Interfaces: Theory and Practice, by Bowman et. al.
                 </td>
             </tr>
             <tr>
-                <td>Pendukung: </td>
-                <td colspan="6">
+                <td class="font-weight-bold bg-light">Pendukung: </td>
+                <td colspan="6 ">
                     1. IEEE Transactions on Computational Intelligence and AI in Games<br> 2. IEEE Transactions Systems, Man and Cybernetics
                 </td>
             </tr>
             <tr>
-                <td>Dosen Pengampu</td>
-                <td colspan="7">
+                <td class="font-weight-bold">Dosen Pengampu</td>
+                <td colspan="7 ">
                     Darlis Herumurti, Hadziq Fabroyir
                 </td>
             </tr>
             <tr>
-                <td>Matakuliah syarat</td>
-                <td colspan="7">
+                <td class="font-weight-bold">Matakuliah syarat</td>
+                <td colspan="7 ">
                     -
                 </td>
             </tr>
         </table>
+
+        <h2>Silabus</h2>
+        <table class="table table-bordered ">
+            <thead class="text-center" style="background-color: #deeaf6; ">
+                <tr>
+                    <th>Tatap muka ke-</th>
+                    <th>Kemampuan Akhir Sub CP MK</th>
+                    <th>Keluasan (Materi Pembelajaran)</th>
+                    <th>Metode Pembelajaran</th>
+                    <th>Estimasi Waktu</th>
+                    <th>Kriteria dan Indikator Penilaian</th>
+                    <th>Pengalaman Belajar Mahasiswa</th>
+                    <th>Bobot</th>
+                </tr>
+            </thead>
+            <tbody>
+                <td class="font-weight-bold">1, 2</td>
+                <td>Mahasiswa mampu menjelaskan topik terkait game yang kompleks</td>
+                <td>Game kompleks</td>
+                <td>Kuliah pengantar, diskusi & brainstorming</td>
+                <td>TM = 300’<br>PT = 360’</td>
+                <td>Kejelasan presentasi dan keaktifan mahasiswa dalam kuliah</td>
+                <td>Presentasi: definisi game, sejarah dan perkembangan, game modern</td>
+                <td>4%</td>
+            </tbody>
+        </table>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js " integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj " crossorigin="anonymous "></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js " integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns " crossorigin="anonymous "></script>
 </body>
 
 </html>
