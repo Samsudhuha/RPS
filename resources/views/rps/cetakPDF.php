82d760b2b73168329a1409d788f0249dc8d21b44
Ryukazu <ryukazuas@gmail.com>
2021-03-26 10:14:19 +0700

    tambah cetak rps statis

--- resources/views/rps/cetakPDF.php
+++ resources/views/rps/cetakPDF.php
@@ -2,82 +2,122 @@
 <html lang="en">
 
 <head>
-    <title>Membuat Laporan PDF Dengan DOMPDF Laravel</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 
 <body>
-    <style type="text/css">
-        table tr td,
-        table tr th {
-            font-size: 9pt;
-        }
-    </style>
-    <center>
-        <h5>Rencana Pembelajaran Semester - Algoritma dan Struktur Data</h5>
-    </center>
-
-    <table class='table table-bordered'>
-        <thead>
+    <div class="container">
+        <table class="table table-bordered">
             <tr>
-                <th>#</th>
-                <th>Kode/Matakuliah</th>
-                <th>Kelas</th>
-                <th>Jam</th>
-                <th>SKS</th>
-                <th>Jenis MK</th>
-                <th>Dosen Pengembang RPS</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>VI42103 : Algoritma dan Struktur Data</td>
-                <td>1</td>
-                <td>2</td>
-                <td>2</td>
-                <td>Matakuliah Teori - Keahlian</td>
-                <td>Tri Hadiah Muliawati</td>
+                <td><img src="logo.jpg" alt="" height=100 width=100></td>
+                <th colspan="6" style="text-align:center">INSTITUT TEKNOLOGI SEPULUH NOPEMBER (ITS)<br>Fakultas Teknologi Elektro dan Informatika Cerdas<br>Departemen Teknik Informatika</th>
+                <td>Kode Surat</td>
             </tr>
             <tr>
-                <td>1</td>
-                <td>VI42104 : Sistem Operasi</td>
-                <td>1</td>
-                <td>2</td>
-                <td>2</td>
-                <td>Matakuliah Teori - Keahlian</td>
-                <td>Fitri Setyorini</td>
+                <td colspan="8" style="text-align:center">
+                    <h4>Rencana Pembelajaran Semester</h4>
+                </td>
             </tr>
             <tr>
-                <td>1</td>
-                <td>VI42105 : Basis Data</td>
-                <td>1</td>
-                <td>2</td>
-                <td>2</td>
-                <td>Matakuliah Teori - Keahlian</td>
-                <td>Arif Basori</td>
+                <td colspan="2">Mata Kuliah (MK)</td>
+                <td>KODE</td>
+                <td>Rumpun MK</td>
+                <td colspan="2">BOBOT (sks)</td>
+                <!--<td></td>-->
+                <td>SEMESTER</td>
+                <td colspan="2">Tanggal Penyusunan</td>
+                <!--<td></td>-->
             </tr>
             <tr>
-                <td>1</td>
-                <td>VI42106 : Metode Numerik</td>
-                <td>1</td>
-                <td>2</td>
-                <td>2</td>
-                <td>Matakuliah Teori - Keahlian</td>
-                <td>Ira Prasetyaningrum</td>
+                <td colspan="2">Topik Dalam Pengembangan Game, Realitas Virtual & Augmentasi (TD PGRVA)</td>
+                <td>IF185932</td>
+                <td>Grafika, Interaksi, dan Games</td>
+                <td>3</td>
+                <td>-</td>
+                <td>3</td>
+                <td>10 Mei 2020</td>
             </tr>
             <tr>
-                <td>1</td>
-                <td>VI42107 : Pemrograman Web</td>
-                <td>1</td>
-                <td>2</td>
-                <td>2</td>
-                <td>Matakuliah Teori - Keahlian</td>
-                <td>Isbat Uzzin Nadhlori</td>
+                <td colspan="2" rowspan="2">OTORISASI / PENGESAHAN</td>
+                <td colspan="2">Dosen Pengembang RPS</td>
+                <td colspan="2">Koordinator RMK</td>
+                <td colspan="2">Ka PRODI</td>
             </tr>
-        </tbody>
-    </table>
-
+            <tr>
+                <td colspan="2">Dr. Eng Darlis Herumurti</td>
+                <td colspan="2">Imam Kuswardayan, M.T</td>
+                <td colspan="2">Dr. Eng Chastine Fatichah</td>
+            </tr>
+            <tr>
+                <td rowspan="5">Capaian Pembelajaran</td>
+                <td colspan="7">CPL-Prodi yang dibebankan pada MK</td>
+            </tr>
+            <tr>
+                <td>CPL 4</td>
+                <td colspan="6">Mampu memodelkan, menganalisa, dan mengembangkan aplikasi menggunakan prinsip-prinsip grafika komputer serta interaksi manusia dan komputer</td>
+            </tr>
+            <tr>
+                <td>CPL 4</td>
+                <td colspan="6">Mampu bekerja dan berkomunikasi secara efektif baik secara individu maupun dalam kelompok</td>
+            </tr>
+            <tr>
+                <td colspan="7">Capaian Pembelajaran Mata Kuliah (CPMK)</td>
+            </tr>
+            <tr>
+                <td>CPMK 1</td>
+                <td colspan="6">Mahasiswa mampu menganalisa dan mengklasifikasikan game berdasarkan genre, tema dan rating.</td>
+            </tr>
+            <tr>
+                <td>Peta CPL - CP MK</td>
+                <td colspan="7">Bagian untuk Peta CPL dan CPMK</td>
+            </tr>
+            <tr>
+                <td>Deskripsi Singkat MK</td>
+                <td colspan="7">Pada mata kuliah ini mahasiswa akan belajar mengenai tahapan pengembangan sebuah game yang edukatif dan penelitian yang terkait dengan interaksi manusia komputer secara spesifik di domain realitas virtual dan augmentasi. Mahasiswa diharapkan
+                    mampu merancang, menganalisis serta mengaplikasikan metode-metode pengembangan game pada permasalahan nyata serta mengangkat ide-ide segar dalam suatu penelitian interaksi manusia komputer pada domain realitas virtual dan augmentasi
+                    dengan pendekatan multidisipliner baik secara mandiri atau kerjasama tim.
+                </td>
+            </tr>
+            <tr>
+                <td>Bahan Kajian: Materi Pembelajaran</td>
+                <td colspan="7">
+                    1. HCI/New Interactive Technologies<br> 2. HCI/Mixed, Augmented and Virtual Reality
+                </td>
+            </tr>
+            <tr>
+                <td rowspan="3">Pustaka</td>
+                <td>Utama: </td>
+                <td colspan="6">
+                    1. The VR Book: Human-Centered Design for Virtual Reality by Jason Jerald<br> 2. 3D User Interfaces: Theory and Practice, by Bowman et. al.
+                </td>
+            </tr>
+            <tr>
+                <td>Pendukung: </td>
+                <td colspan="6">
+                    1. IEEE Transactions on Computational Intelligence and AI in Games<br> 2. IEEE Transactions Systems, Man and Cybernetics
+                </td>
+            </tr>
+            <tr>
+                <td>Dosen Pengampu</td>
+                <td colspan="7">
+                    Darlis Herumurti, Hadziq Fabroyir
+                </td>
+            </tr>
+            <tr>
+                <td>Matakuliah syarat</td>
+                <td colspan="7">
+                    -
+                </td>
+            </tr>
+        </table>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 </body>
 
 </html>
